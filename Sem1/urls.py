faf20441ab08a7898544e3258545d740652a837e
naruto-sys <gavriil.belov@yandex.ru>
2023-10-31 14:38:54 +0300

    Update task-2 files

--- Sem1/urls.py
+++ Sem1/urls.py
@@ -16,7 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from App import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls,),
+    path('', views.index, name='index'),
+    path('question/<int:question_id>', views.question, name='question'),
+    path('hot', views.hot, name='hot'),
+    path('tag/<str:tag_name>', views.tag, name='tag'),
+    path('login', views.login, name='login'),
+    path('signup', views.signup, name='signup'),
+    path('ask', views.ask, name='ask'),
+    path('profile', views.profile, name='profile'),
 ]
